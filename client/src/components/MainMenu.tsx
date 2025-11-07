cc3d3d5d62afc05dc18f77215b8eb617b04115c4
akosfodor <akosfodor10@gmail.com>
2025-11-07 16:26:02 +0100

    logout

--- client/src/components/MainMenu.tsx
+++ client/src/components/MainMenu.tsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const MainMenu = () => {
   const [userName, setUserName] = useState<string | null>(null);
+  const navigate = useNavigate()
 
   useEffect(() => {
     const storedUser = localStorage.getItem("userName");
     setUserName(storedUser);
   }, []);
 
+  const logout = async () => {
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    if (refreshToken) {
+        await fetch("http://localhost:5136/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(refreshToken),
+        });
+    }
+
+    localStorage.clear();
+    navigate("/");
+    };
+
   return (
     <>
     <div className="text-white text-2xl">
-      {userName ? `Üdv, ${userName}` : "Mittomán"}
+      Üdv, {userName}
     </div>
     <button
-        onClick={() => {
-            localStorage.removeItem("username");
-            localStorage.removeItem("token");
-            localStorage.removeItem("refreshToken");
-            window.location.href = "/";
-        }}
+        onClick={logout}
         className="mt-4 px-4 py-2 bg-red-600 rounded cursor-pointer"
         >
         Kijelentkezés
